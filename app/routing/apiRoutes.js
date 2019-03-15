fb93e883bd7428e7971b1d3c3f3f9ad49b06dfa1
kanikas01 <larvakurt@yahoo.com>
2019-03-15 15:55:52 -0700

    Add basic post request functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,6 +12,27 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    var userScores = req.body.scores;
+    var bestMatch = friends[0];
+    var bestMatchScore = 40;
+    var totalDifference = 0;
+    friends.forEach(element => {
+      for (var i = 0; i < userScores.length; i++) {
+        console.log(userScores[i], element.scores[i]);
+        totalDifference += Math.abs(userScores[i] - element.scores[i]);
+      }
+
+      if (totalDifference < bestMatchScore) {
+        bestMatch = element;
+        bestMatchScore = totalDifference;
+      }
+
+    console.log(totalDifference);
+    totalDifference = 0;
+    });
+    
+    console.log(bestMatch.name);
+    res.json(bestMatch);
 
   });
 }
