07d35cf84d910384cfc1f0964a5a6f2253bf2902
kanikas01 <larvakurt@yahoo.com>
2019-03-15 17:50:43 -0700

    Add photo links for friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends =	[
   {
-    "name": "Alpha",
-    "photo": "",
+    "name": "Andy",
+    "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE1ODMzMzM3MzU1/the-andy-griffith-show.jpg",
     "scores": [
       "1",
       "1",
@@ -16,8 +16,8 @@ var friends =	[
     ]
   },
   {
-    "name": "Bravo",
-    "photo": "",
+    "name": "Betty",
+    "photo": "https://starcasm.net/wp-content/uploads/2010/03/bettywhiteyoung.jpg",
     "scores": [
       "5",
       "4",
@@ -32,8 +32,8 @@ var friends =	[
     ]
   },
   {
-    "name": "Charlie",
-    "photo": "",
+    "name": "Chico",
+    "photo": "http://2.bp.blogspot.com/-ovXMGbAGmQ4/VYqtTUHXWtI/AAAAAAAACMI/ogkb5rT8j7k/s1600/Chico-Marx-39.jpg",
     "scores": [
       "1",
       "2",
@@ -49,7 +49,7 @@ var friends =	[
   },
   {
     "name": "Delta",
-    "photo": "",
+    "photo": "http://d3m2ca683sarz5.cloudfront.net/wp-content/uploads/2018/11/15115944/0ec095e7200d1f6ac6e89f29dd463bda.png",
     "scores": [
       "5",
       "5",
@@ -64,8 +64,8 @@ var friends =	[
     ]
   },
   {
-    "name": "Echo",
-    "photo": "",
+    "name": "Egon",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/3/37/Harold_Ramis_as_Egon_Spengler.jpg",
     "scores": [
       "1",
       "3",
